bf44292bc148a98a42caa62a787336c36a31cdd9
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2023-08-19 16:21:22 -0300

    fix - adicionando enum ao produtoRelatorioDTO

--- src/main/java/br/com/dbc/vemser/ecommerce/dto/produto/ProdutoRelatorioDTO.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/dto/produto/ProdutoRelatorioDTO.java
@@ -1,6 +1,7 @@
 package br.com.dbc.vemser.ecommerce.dto.produto;
 
 
+import br.com.dbc.vemser.ecommerce.entity.enums.TipoSetor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProdutoRelatorioDTO {
-
     private String modelo;
-    private String setor;
+    private TipoSetor setor;
     private Double valor;
 }
