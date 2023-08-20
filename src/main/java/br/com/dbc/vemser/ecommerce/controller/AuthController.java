746367663d69d81892f79e9f7062b0a36f5927ce
ricardo <rosastaricardo84@gmail.com>
2023-08-20 17:54:46 -0300

    feat[authController]: atualizacao-desativacao-log usuario

--- src/main/java/br/com/dbc/vemser/ecommerce/controller/AuthController.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/controller/AuthController.java
@@ -1,11 +1,14 @@
 package br.com.dbc.vemser.ecommerce.controller;
 
 import br.com.dbc.vemser.ecommerce.dto.usuario.LoginDTO;
+import br.com.dbc.vemser.ecommerce.dto.usuario.UserAtualizacaoDTO;
 import br.com.dbc.vemser.ecommerce.entity.UsuarioEntity;
 import br.com.dbc.vemser.ecommerce.exceptions.RegraDeNegocioException;
 import br.com.dbc.vemser.ecommerce.security.TokenService;
 import br.com.dbc.vemser.ecommerce.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +26,7 @@ public class AuthController {
     public final AuthenticationManager authenticationManager;
     private final TokenService tokenService;
     private final UsuarioService usuarioService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping
     public String auth(@RequestBody @Valid LoginDTO loginDTO) throws RegraDeNegocioException {
@@ -43,6 +48,43 @@ public class AuthController {
     @PostMapping("/cadastro")
     public LoginDTO cadastro(@RequestBody LoginDTO user, @RequestParam(value = "role", required = false) Integer role) throws RegraDeNegocioException {
         return usuarioService.cadastro(user, role);
+    }
+
+    @GetMapping("/usuario-logado")
+    public UsuarioEntity recuperarUsuarioLogado() throws RegraDeNegocioException {
+
+
+        return usuarioService.getLoggedUser();
+    }
+
+    @PutMapping("/usuario-trocar-senha")
+    public ResponseEntity<Void> recuperarUsuarioLogado(@RequestBody LoginDTO loginDTO) throws RegraDeNegocioException {
+
+        usuarioService.atualizarSenha(loginDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/desativar-usuario/{login}")
+    public ResponseEntity<Void> desativarUsuarioLogado(@NotBlank(message = "Informe o login.")
+                                                       @PathVariable String login)
+                                                       throws RegraDeNegocioException {
+
+        usuarioService.desativarUsuario(login);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PutMapping("/atualizar-usuario/{login}")
+    public ResponseEntity<Void> atualizarUsuario(@NotBlank(message = "Informe o login.")
+                                                 @PathVariable String login,
+                                                 @RequestBody UserAtualizacaoDTO userAtualizacaoDTO)
+            throws RegraDeNegocioException {
+
+        usuarioService.atualizarUsuario(login, userAtualizacaoDTO);
+
+        return ResponseEntity.ok().build();
     }
 
 }
