50a1cbfc9cac92496312ca39a4401d845e8e1db8
J3ffn <jefferson.mangueira12@gmail.com>
2023-08-23 00:13:28 -0300

    fix: RegraDeNegÃocioException dando prioridade ao HashMap

--- src/main/java/br/com/dbc/vemser/ecommerce/exceptions/RegraDeNegocioException.java
+++ src/main/java/br/com/dbc/vemser/ecommerce/exceptions/RegraDeNegocioException.java
@@ -3,17 +3,8 @@ package br.com.dbc.vemser.ecommerce.exceptions;
 
 public class RegraDeNegocioException extends Exception {
 
-//    private Map<String, String> camposViolados;
-
     public RegraDeNegocioException(String mensagemDeErro) {
         super(mensagemDeErro);
     }
 
-//    public RegraDeNegocioException(Map<String, String> camposViolados) {
-//        this.camposViolados = camposViolados;
-//    }
-//
-//    public Map<String, String> getCamposViolados() {
-//        return camposViolados;
-//    }
 }
